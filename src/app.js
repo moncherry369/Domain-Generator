7189823250ead37542f9dadd46b1e89cfd46966b
moncherry369 <ssgarrido1994@gmail.com>
2023-02-07 23:40:56 +0000

    Final touches on domain generator. button working

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generateExcuse() {
+  // Who Generator //
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let noun = ["the", "our", "we"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let shuffle = Math.random();
+  let totalNoun = noun.length;
+  let shuffleIndex = Math.floor(shuffle * totalNoun);
+  let randomNoun = noun[shuffleIndex];
+
+  // ACTION GENERATOR //
+
+  let adj = ["wonderful", "fantastic", "silly"];
+
+  let shuffleAdj = Math.random();
+  let totalAdj = adj.length;
+  let shuffleAdjIndex = Math.floor(shuffleAdj * totalAdj);
+  let randomAdj = adj[shuffleAdjIndex];
+
+  // WHAT GENERATOR //
+
+  let obj = ["apples", "balloons", "turtles"];
+
+  let shuffleObj = Math.random();
+  let totalObj = obj.length;
+  let shuffleObjIndex = Math.floor(shuffleObj * totalObj);
+  let randomObj = obj[shuffleObjIndex];
+
+  // WHEN GENERATOR //
+
+  let ext = [".com", ".net", ".io"];
+  let shuffleExt = Math.random();
+  let totalExt = ext.length;
+  let shuffleExtIndex = Math.floor(shuffleExt * totalExt);
+  let randomExt = ext[shuffleExtIndex];
+  let excuseElement = document.querySelector(".domain");
+
+  excuseElement.innerHTML = randomNoun + randomAdj + randomObj + randomExt;
+  console.log(excuseElement.innerHTML);
+}
+
+window.onload = generateExcuse();
